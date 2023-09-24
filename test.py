3ce62bc2138486f0f7f02986ee4ac736f7657515
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2023-09-25 03:05:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import random
 
-# Global variables
+# Global variables 
+'''
+Played moves sets all the moves made in game in a list, last opponents move
+saves the last move
+'''
 played_moves = set()
 last_opponent_move = ""
 
+'''
+convert gtp to coords converts the gtp coordinates into x and y coordinates to manupulate
+'''
 def convert_gtp_to_coords(gtp_coords):
     column, row = gtp_coords[0], gtp_coords[1:]
     x = ord(column) - ord('A')
@@ -12,11 +19,21 @@ def convert_gtp_to_coords(gtp_coords):
     y = 19 - int(row)
     return x, y
 
+'''
+play gets the last move and the moves played and adds the move played for the colour
+and changes the last move played. Its and update system
+'''
+
 def play(color, move):
     global played_moves, last_opponent_move
     played_moves.add(move)
     last_opponent_move = move
     return "="
+
+'''
+genmove tries to generate a mirror move, if not successful because already played
+then it will find the first random move in the not played and play it, if the board is filled then it will pass
+'''
 
 def genmove(color):
     # Try mirrored move first
@@ -36,12 +53,23 @@ def genmove(color):
     # If all intersections are occupied, pass
     return "= pass"
 
+'''
+mirror move, mirrors the moves made by reversing the cordinates then adding them beack into gtp coords 
+then returning it
+'''
+
 def mirror_move(move):
     x, y = convert_gtp_to_coords(move)
     mirror_x = 18 - x
     mirror_y = 18 - y
     mirrored_move = chr(mirror_x + ord('A')) + str(19 - mirror_y)
     return mirrored_move
+
+'''
+Handle command, handles all the commands Sabaki will send to the bot
+including the name and playing the moves, by spliting and using the list to check
+what is being asked
+'''
 
 def handle_command(command):
     tokens = command.strip().split()
@@ -79,6 +107,11 @@ def handle_command(command):
         return "="
 
     return "? unknown command"
+
+'''
+This is the main function that will force a while loop to run until the command says quit, then it will break, or 
+it will break if there is an error
+'''
 
 if __name__ == "__main__":
     while True:
